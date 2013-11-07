70aef8f0ce88a32f7ad63cd55cbb569bf9083e4d
Matt Catrett <mcatrett@turbosquid.com>
2013-11-07 11:21:03 -0600

    in progress backtracking algo

--- queens/nqueens.rb
+++ queens/nqueens.rb
@@ -1,17 +1,17 @@
 module NQueens
   class Solver
-    def self.solve(n=8, show_board=true, strategy=BruteForceStrategy)
+    def self.solve(n=8, show_board=false, strategy=BruteForceStrategy)
       strategy.solve(n, show_board)
     end
   end
 
   class Strategy
-    def self.solve(n=8, show_board=true)
+    def self.solve(n=8, show_board=false)
     end
   end
 
   class BruteForceStrategy < Strategy
-    def self.solve(n=8, show_board=true)
+    def self.solve(n=8, show_board=false)
       raise if n < 1
 
       t1 = Time.now
@@ -38,6 +38,61 @@ module NQueens
     def self.print(vector, n)
       vector.each { |i| puts "#{'| ' * (i-1)}|Q#{'| ' * (n - i)}|" }
       puts ""
+    end
+  end
+
+  class BacktrackingStrategy < Strategy
+    def self.solve(n=8, show_board=false)
+      raise if n < 1
+
+      t1 = Time.now
+      solutions = 0
+      backtracks = 0
+      board = []
+      queens = []
+      position = 0
+
+      neighbors = precompute(n)
+
+      (n**2).times { board << 0 }
+
+      #attack = 
+
+    end
+
+    private
+
+    def self.attack(position, delta=1)
+      
+    end
+
+    def self.precompute(n=8)
+      neighbors = []
+
+      (0..n**2).each do |position|
+        neighbors << find_neighbors(position)
+      end
+
+      neighbors
+    end
+
+    def self.find_neighbors(position, n=8)
+      a = []
+      row = (position / n).floor
+      column = position % n
+      (0..n).each do |i|
+        unless i == column
+          a << row * n + i
+          r1 = row + column - i
+          r2 = row + i - column
+          a << r1 * n + i if r1 >= 0 && r1 < n
+          a << r2 * n + i if r2 >= 0 && r2 < n
+        end
+        unless i == row
+          a << i * n + column
+        end
+      end
+      a
     end
   end
 end
